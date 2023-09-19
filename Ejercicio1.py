369dfe4765ec92062156dd8aa8d02b7a47ea5f29
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2023-09-19 14:00:44 -0700

    new file:   Ejercicio1.py

---
+++ Ejercicio1.py
@@ -0,0 +1,11 @@
+#19/09/2023 Unidad 2 Trabajo en clase implementacion en pyhton
+"""COMENTARIO LARGOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOo
+aaaaa Ya le supe a wilson """
+
+import matplotlib.pyplot as plt
+x=[5,7,8,7,2,17,2,9,4,11,12,9,6] #numeoro de vendedores
+
+y=[99,86,87,88,111,86,103,87,94,78,77,85,86] #ventas en miles
+
+plt.scatter(x, y)
+plt.show()
